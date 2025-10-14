c29d304ec61115e8a220d3d83864fe952be5de8d
abdulamnaser-bit <abdul.amnaser@gmail.com>
2025-10-14 12:27:40 +0530

    Add files via upload

---
+++ task 1b naser.py
@@ -0,0 +1,14 @@
+# Program b: Relational Operations
+# Enter two numbers and perform relational operations
+
+# Input from user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Relational operations
+print("Is num1 > num2?", num1 > num2)
+print("Is num1 < num2?", num1 < num2)
+print("Is num1 == num2?", num1 == num2)
+print("Is num1 != num2?", num1 != num2)
+print("Is num1 >= num2?", num1 >= num2)
+print("Is num1 <= num2?", num1 <= num2)
